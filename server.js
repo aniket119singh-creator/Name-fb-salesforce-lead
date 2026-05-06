2ad1b8ad18237e640f35319013b3839a554fcac3
aniket119singh-creator <aniket119singh@gmail.com>
2026-05-06 12:26:12 +0530

    server.js

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ const VERIFY_TOKEN = 'facebook123';
 
 app.get('/webhook', (req, res) => {
 
+    console.log('GET WEBHOOK CALLED');
+
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
     if (mode && token === VERIFY_TOKEN) {
+
         console.log('WEBHOOK VERIFIED');
+
         return res.status(200).send(challenge);
     }
 
@@ -22,10 +26,16 @@ app.get('/webhook', (req, res) => {
 
 app.post('/webhook', (req, res) => {
 
-    console.log('================ NEW LEAD =================');
+    console.log('==============================');
+    console.log('NEW FACEBOOK LEAD RECEIVED');
     console.log(JSON.stringify(req.body, null, 2));
+    console.log('==============================');
 
-    res.sendStatus(200);
+    return res.status(200).send('EVENT_RECEIVED');
+});
+
+app.get('/', (req, res) => {
+    res.send('Server Running');
 });
 
 const PORT = process.env.PORT || 3000;
